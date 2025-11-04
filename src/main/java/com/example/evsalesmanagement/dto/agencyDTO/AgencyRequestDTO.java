68b79fd2f9ad7ed8d87194702d218aa4998ec380
dinhfu05 <dinhphu511205@gmail.com>
2025-11-04 22:20:22 +0700

    fix(daiLy) : sửa các method trả về dto

---
+++ src/main/java/com/example/evsalesmanagement/dto/agencyDTO/AgencyRequestDTO.java
@@ -0,0 +1,57 @@
+package com.example.evsalesmanagement.dto.agencyDTO;
+
+public class AgencyRequestDTO {
+
+    private String agencyName;   
+
+    private String address;   
+
+    private String phoneNumber;    
+
+    private String email;    
+
+    private String status;
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }    
+
+}
+
+    
