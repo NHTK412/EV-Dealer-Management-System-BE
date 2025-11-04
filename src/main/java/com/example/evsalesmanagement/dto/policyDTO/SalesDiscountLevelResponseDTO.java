9eacb784640893e28cad5a94a1ef1fa21692d37c
dinhfu05 <dinhphu511205@gmail.com>
2025-11-04 21:48:46 +0700

    feat(chietkhau): thêm api crud cho chiết khấu #24

---
+++ src/main/java/com/example/evsalesmanagement/dto/policyDTO/SalesDiscountLevelResponseDTO.java
@@ -0,0 +1,37 @@
+package com.example.evsalesmanagement.dto.policyDTO;
+
+import java.math.BigDecimal;
+
+public class SalesDiscountLevelResponseDTO {
+    private Integer salesDiscountLevelId;
+    private Integer salesFrom;
+    private Integer salesTo;
+    private BigDecimal discountPercentage;
+
+    public Integer getSalesDiscountLevelId() {
+        return salesDiscountLevelId;
+    }
+    public void setSalesDiscountLevelId(Integer salesDiscountLevelId) {
+        this.salesDiscountLevelId = salesDiscountLevelId;
+    }
+    public Integer getSalesFrom() {
+        return salesFrom;
+    }
+    public void setSalesFrom(Integer salesFrom) {
+        this.salesFrom = salesFrom;
+    }
+    public Integer getSalesTo() {
+        return salesTo;
+    }
+    public void setSalesTo(Integer salesTo) {
+        this.salesTo = salesTo;
+    }
+    public BigDecimal getDiscountPercentage() {
+        return discountPercentage;
+    }
+    public void setDiscountPercentage(BigDecimal discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+    
+    
+}
