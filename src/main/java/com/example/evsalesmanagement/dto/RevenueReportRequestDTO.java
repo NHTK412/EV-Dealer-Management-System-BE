713e6dc5831aa87e465b4168e760e6401f5f98fa
Giabao11082005 <hoanggiabao110820055@gmail.com>
2025-11-04 22:19:33 +0700

    feat(revenue): hoàn thiện chức năng báo cáo doanh thu và xuất báo cáo với Excel

---
+++ src/main/java/com/example/evsalesmanagement/dto/RevenueReportRequestDTO.java
@@ -0,0 +1,75 @@
+package com.example.evsalesmanagement.dto;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+
+
+public class RevenueReportRequestDTO {
+
+    @NotNull(message = "agencyId must not be null")
+    private Integer agencyId;
+
+    @NotNull(message = "vehicleTypeId must not be null")
+    private Integer vehicleTypeId;
+
+    @Pattern(
+        regexp = "^(Completed|In process|Pending)?$",
+        message = "Status is invalid. Allowed values are: Completed, In process, Pending"
+    )
+    private String status;
+
+    @NotNull(message = "fromDate must not be null")
+    @PastOrPresent(message = "fromDate must be in the past or present")
+    private LocalDateTime fromDate;
+
+    @NotNull(message = "toDate must not be null")
+    @PastOrPresent(message = "toDate must be in the past or present")
+    private LocalDateTime toDate;
+
+    public RevenueReportRequestDTO() {}
+
+    // Getters and Setters
+    public void setAgencyId(Integer agencyId) {
+        this.agencyId = agencyId;
+    }
+
+    public void setVehicleTypeId(Integer vehicleTypeId) {
+        this.vehicleTypeId = vehicleTypeId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setFromDate(LocalDateTime fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public void setToDate(LocalDateTime toDate) {
+        this.toDate = toDate;
+    }
+
+    public Integer getAgencyId() {
+        return agencyId;
+    }
+
+    public Integer getVehicleTypeId() {
+        return vehicleTypeId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getFromDate() {
+        return fromDate;
+    }
+
+    public LocalDateTime getToDate() {
+        return toDate;
+    }
+}
