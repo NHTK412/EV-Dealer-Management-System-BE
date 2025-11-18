1f58261e7a02318263c8ed306f5e04e2bfc47ceb
nphuowng205 <phuongww111@gmail.com>
2025-11-19 02:51:30 +0700

    fix(theo dõi tình trạng giao hàng): thêm endpoint put và post

--- src/main/java/com/example/evsalesmanagement/repository/VehicleDeliveryRepository.java
+++ src/main/java/com/example/evsalesmanagement/repository/VehicleDeliveryRepository.java
@@ -1,21 +1,18 @@
 package com.example.evsalesmanagement.repository;
 
+import com.example.evsalesmanagement.enums.VehicleDeliveryStatusEnum;
 import com.example.evsalesmanagement.model.VehicleDelivery;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import com.example.evsalesmanagement.model.Order;
 
 import java.util.List;
 
 @Repository
 public interface VehicleDeliveryRepository extends JpaRepository<VehicleDelivery, Integer> {
 
-    @Query("SELECT v FROM VehicleDelivery v WHERE LOWER(v.status) = LOWER('Pending')")
-    List<VehicleDelivery> findPendingDeliveries();
+    boolean existsByOrderIdAndStatusIn(Order order, List<VehicleDeliveryStatusEnum> statuses);
 
-    @Query("SELECT v FROM VehicleDelivery v WHERE LOWER(v.status) = LOWER('In Progress')")
-    List<VehicleDelivery> findInProgressDeliveries();
-
-    @Query("SELECT v FROM VehicleDelivery v WHERE LOWER(v.status) = LOWER('Completed')")
-    List<VehicleDelivery> findCompletedDeliveries();
+    List<VehicleDelivery> findByStatus(VehicleDeliveryStatusEnum status);
+    
 }
